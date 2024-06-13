256a4510b41c3c6912657a147244c49bed6cd003
jamakr4 <68536642+jamakr4@users.noreply.github.com>
2024-06-13 14:48:49 +0200

    Icons added to index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { ShopLandingPageComponent } from './shop-landing-page/shop-landing-page.component';
 import { PlantDetailShopPageComponent } from './shop-detail-page/plant-detail-shop-page.component';
+import { TaskbarComponent } from './taskbar/taskbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ShopLandingPageComponent,PlantDetailShopPageComponent,RouterLink],
+  imports: [RouterOutlet,ShopLandingPageComponent,PlantDetailShopPageComponent,RouterLink,TaskbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
