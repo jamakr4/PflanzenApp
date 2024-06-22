0f78d63e637ffc660d37775608c38069f56ff1e2
TimoWlekli <timo.Wleklinski@gmx.de>
2024-06-22 17:18:44 +0200

    MeinePflanzen(Startseite)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { BurgermenuComponent } from './burgermenu/burgermenu.component';
 import { DiaryChangeComponent } from './diary-change/diary-change.component';
 import { DiaryIndexComponent} from './diary-index/diary-index.component';
 import { DiaryInputComponent } from './diary-input/diary-input.component';
+import { MyPlantsComponent } from './myplants/myplants.component';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 
@@ -48,5 +49,8 @@ export const routes: Routes = [
     {
 
         path:"diary-index-component", component: DiaryIndexComponent
+    },
+    {
+        path:"myplants", component:MyPlantsComponent
     }
 ];
