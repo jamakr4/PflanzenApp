6eedd41bd59dfd61093e3627bfcf56d61c711ce0
RobertLukasik <robert-alexander.lukasik@hsrw.org>
2024-06-23 21:11:32 +0200

    Login_done_Robert_Lukasik

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
 })
