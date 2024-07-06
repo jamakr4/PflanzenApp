96b05734706178dab10df492fc0e02212635b4c9
jamakr4 <68536642+jamakr4@users.noreply.github.com>
2024-07-06 12:54:16 +0200

    Local Storage Error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ShopLandingPageComponent } from './shop-landing-page/shop-landing-page.component';
 import { PlantDetailShopPageComponent } from './shop-detail-page/plant-detail-shop-page.component';
@@ -6,7 +6,7 @@ import { TaskbarComponent } from './taskbar/taskbar.component';
 import { BackgroundimageComponent } from './backgroundimage/backgroundimage.component';
 import { HeaderComponent } from './header/header.component';
 import { QuizLandingPageComponent } from './quiz-landing-page/quiz-landing-page.component';
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule, NgIf, isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
@@ -22,7 +22,7 @@ import { Router } from '@angular/router';
     TaskbarComponent,
     BackgroundimageComponent,
     HeaderComponent,
-    QuizLandingPageComponent
+    QuizLandingPageComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -30,7 +30,7 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
   currentMode: 'light' | 'dark' = 'light';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   isAuthPage(): boolean {
     return ['/login', '/registration', '/prelogin', '/'].includes(this.router.url);
@@ -39,19 +39,25 @@ export class AppComponent implements OnInit {
   auth = true;
 
   ngOnInit(): void {
-    this.currentMode = (localStorage.getItem('colorMode') as 'light' | 'dark') || 'dark';
-    this.applyMode();
+    if (isPlatformBrowser(this.platformId)) {
+      this.currentMode = (localStorage.getItem('colorMode') as 'light' | 'dark') || 'dark';
+      this.applyMode();
+    }
   }
 
   setMode(mode: 'light' | 'dark'): void {
     this.currentMode = mode;
-    localStorage.setItem('colorMode', mode);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('colorMode', mode);
+    }
     this.applyMode();
   }
 
   applyMode(): void {
-    document.body.classList.remove('light-mode', 'dark-mode');
-    document.body.classList.add(`${this.currentMode}-mode`);
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.remove('light-mode', 'dark-mode');
+      document.body.classList.add(`${this.currentMode}-mode`);
+    }
   }
 
   title = 'PflanzenApp';
