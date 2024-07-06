59e7ea849ab4c9b48a36be553cb39c7c8ed9a1a8
RobertLukasik <robert-alexander.lukasik@hsrw.org>
2024-07-06 21:42:05 +0200

    rl_33932_save

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Plant } from '../classes/plants';
+import { PlantService } from '../services/plant.service';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
 export class WishlistComponent {
+
+  
 
 }
