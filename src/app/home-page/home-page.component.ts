f7b23cfcf5d3fa2b64971cf2a2b637482bb38a3a
NicolasHahne <mail.nicolashahne@gmail.com>
2024-07-06 17:46:51 +0200

    pinned plants setup

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { QuoteComponent } from '../quote/quote.component';
 import { WeatherDisplayComponent } from '../weather-display/weather-display.component';
+import { Plant } from '../classes/plants';
+import { PlantService } from '../services/plant.service';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [QuoteComponent, WeatherDisplayComponent],
+  imports: [QuoteComponent, RouterLink, WeatherDisplayComponent, CommonModule, NgIf],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  plants: Plant[] = [];
+  constructor(private plantService: PlantService) {
+
+    this.plants = plantService.getOwnedPlants();
+  }
 }
